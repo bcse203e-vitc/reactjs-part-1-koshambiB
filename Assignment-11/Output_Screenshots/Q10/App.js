c1696890a2efc4817c754b6e42a66158ce801594
Koshambi Bardhan <bardhankoshambi2005@gmail.com>
2025-02-25 21:05:19 +0530

    Create App.js

---
+++ Assignment-11/Output_Screenshots/Q10/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export function App() {
+  return(
+     <img src="https://gratisography.com/wp-content/uploads/2023/03/gratisography-not-happy-in-a-hat-stock-photo-800x525.jpg" alt='photo'/>
+  )
+
+}
+
