41eb6a076a1fed9cacb0ad0c9dfd97df97a69963
Koshambi Bardhan <bardhankoshambi2005@gmail.com>
2025-02-25 20:43:34 +0530

    Create App.js

---
+++ Assignment-11/Output_Screenshots/Q4/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Header";  
+import Footer from "./Footer";  
+
+
+function App() {
+  return(
+    <>
+    <Header />
+    <p>This is the main content. Rendering is shown below</p>
+    <Footer />
+    </>
+  )
+}
+
+export default App;
